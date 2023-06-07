9d985aaa0aca5a40da0881a275df5bed649f74e9
dinarakineyeva <79667897+dinarakineyeva@users.noreply.github.com>
2023-06-07 11:27:04 -0400

    Create tfvars.tf

---
+++ tfvars.tf
@@ -0,0 +1,32 @@
+######################################
+########## MONGO DB ATLAS ############
+######################################
+gcp_project_id = "elaborate-hash-385117"
+mongo_project_id      = "644fa96ebff4ee115f6d7642"
+mongo_public_key      = "snjevltv"
+mongo_private_key     = "a10b55da-105f-4f02-97ed-9349baf17668"
+gcp_region            = "us-central1"
+cloud_provider        = "GCP"
+mongo_cluster_name    = "test-cluster-sandbox"
+mongo_cluster_version = "6.0"
+mongo_cluster_type    = "REPLICASET"
+cloud_backup          = true
+autoscaling           = true
+mongo_cluster_size    = "M0"
+num_shards            = 1
+mongo_cluster_region  = "CENTRAL_US"
+electable_nodes       = 3
+priority              = 7
+read_only_nodes       = 0
+db_username           = "sandboxuser"
+db_password           = "sandboxuser"
+auth_database_name    = "admin"
+db_role_name          = "readWriteAnyDatabase"
+database_name         = "admin"
+db_key                = "key"
+db_value              = "value"
+google_compute_address = "10.0.42."
+google_compute_address_type = "INTERNAL"
+network_name = "sandbox-newtork"
+subnet_name = "sandbox-subnet"
+compute_address_name = "sandbox-mongo"
